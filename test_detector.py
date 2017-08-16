f4fbdd9cd361e6e1b6aa6a1239cc91a023540258
Jacob Bieker <jacob@bieker.tech>
2017-08-16 14:09:08 +0200

    Add test for consistency with same random seed
    This came about from issues with MCMC where using the same random seed, sometimes the there would be an error on NaN errors, but othertimes not with no changes.

--- test_detector.py
+++ test_detector.py
@@ -29,5 +29,49 @@ def test_detector(random_state=None, plot=False):
     assert all(np.isclose(np.sum(detector_matrix, axis=0), 1.))
 
 
+def test_consistency(random_state=None):
+    if not isinstance(random_state, np.random.RandomState):
+        random_state = np.random.RandomState(random_state)
+
+    energies = 1000.0 * random_state.power(0.70, 5000)
+    below_zero = energies < 1.0
+    energies[below_zero] = 1.0
+
+    signal_valuess = []
+    true_hitss = []
+    energies_returns = []
+    detector_matrixs = []
+    for i in range(100):
+        detector = Detector(distribution='gaussian',
+                            energy_loss='const',
+                            make_noise=True,
+                            smearing=True,
+                            resolution_chamber=1.,
+                            noise=0.,
+                            response_bins=20,
+                            rectangular_bins=20,
+                            random_state=random_state)
+        signal, true_hits, energies_return, detector_matrix = detector.simulate(
+            energies)
+        signal_valuess.append(signal)
+        true_hitss.append(true_hits)
+        energies_returns.append(energies_return)
+        detector_matrixs.append(detector_matrix)
+
+    signal_valuess = np.asarray(signal_valuess)
+    true_hitss = np.asarray(true_hitss)
+    energies_returns = np.asarray(energies_returns)
+    detector_matrixs = np.asarray(detector_matrixs)
+
+    for index, array in enumerate(signal_valuess):
+        print(index)
+        print(signal_valuess)
+        print(signal_valuess[index+1])
+        assert np.array_equal(signal_valuess[index], signal_valuess[index+1])
+        assert np.array_equal(true_hitss[index], true_hitss[index+1])
+        assert np.array_equal(energies_returns[index], energies_returns[index+1])
+        assert np.array_equal(detector_matrixs[index], detector_matrixs[index+1])
+
 if __name__ == "__main__":
+    test_consistency(1347)
     test_detector(1347, plot=True)
