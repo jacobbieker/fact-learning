cdfab61e359993e6ebb76d5de8216896e63302d5
Jacob Bieker <jacob@bieker.tech>
2017-08-21 15:30:32 +0200

    Try out modified fact_example
    Sorta works? Not sure what E_RF is, and it doesn't show up at all in the gamma_test I am using, so subsituted it with another value that might or (probably) doesn't make sense

--- fact-data.py
+++ fact-data.py
@@ -13,7 +13,6 @@ from fact.analysis import split_on_off_source_dependent
 import logging
 from configparser import ConfigParser
 
-
 '''
 Parameters for the classic fitting of the gamma data, taken from the TRUEE settings
 # 1st observable
@@ -48,7 +47,7 @@ Run on vollmond probably, since it takes a long time
 log = logging.getLogger("setup_pypet")
 
 df = pd.read_hdf("gamma_precuts.hdf5")
-print(list(df))
+#print(list(df))
 print("+++++++++++++++++++++++++++++++++++++++++++")
 mc_df = read_h5py("gamma_test.hdf5", key='events')
 print(list(mc_df))
@@ -58,11 +57,11 @@ def load_gamma_subset(sourcefile, bins={'gamma_energy_prediction': 25, 'conc_cor
                       theta2_cut=0.0, conf_cut=0.0, num_off_positions=1, analysis_type='classic'):
     events = read_h5py(sourcefile, key='events')
 
-    selection_columns = ['theta_deg', 'gamma_prediction', 'zd_tracking']
+    selection_columns = ['theta_deg', 'gamma_prediction', 'zd_tracking', 'conc_core']
     theta_off_columns = ['theta_deg_off_{}'.format(i)
                          for i in range(1, num_off_positions)]
     bg_prediction_columns = ['gamma_prediction_off_{}'.format(i)
-                         for i in range(1, num_off_positions)]
+                             for i in range(1, num_off_positions)]
 
     if analysis_type == 'source':
         log.info('\tSelection events for source dependent analysis')
@@ -93,4 +92,51 @@ def load_gamma_subset(sourcefile, bins={'gamma_energy_prediction': 25, 'conc_cor
 
     return on_mc, on_data, off_data
 
-load_gamma_subset("gamma_test.hdf5", theta2_cut=0.7, conf_cut=0.3, num_off_positions=5)
+
+if __name__ == '__main__':
+    mc_data, on_data, off_data = load_gamma_subset("gamma_test.hdf5", theta2_cut=0.7, conf_cut=0.3, num_off_positions=5)
+
+    print(on_data.shape)
+    print(off_data.shape)
+    print(mc_data.shape)
+
+    # Get the "test" vs non test data
+    df_test = on_data[10000:]
+    df_train = on_data[:10000]
+
+
+    '''
+
+    X = df_train.get(['conc_core', 'gamma_energy_prediction']).values
+    X_test = df_test.get(['conc_core', 'gamma_energy_prediction']).values
+
+    binning_E = np.linspace(2.4, 4.2, 10)
+    binned_E = np.digitize(df_train.corsika_evt_header_total_energy,
+                           binning_E)
+    binned_E_test = np.digitize(df_test.corsika_evt_header_total_energy,
+                                binning_E)
+    classic_binning = ff.discretization.ClassicBinning(
+        bins = [15, 25])
+    print(classic_binning.fit(X))
+
+    fig, ax = plt.subplots()
+    ff.discretization.visualize_classic_binning(ax,
+                                             classic_binning,
+                                             X,
+                                             log_c=False,
+                                             cmap='viridis')
+    fig.savefig('05_fact_example_original_binning.png')
+
+    closest = classic_binning.merge(X_test,
+                                    min_samples=10,
+                                    max_bins=None,
+                                    mode='closest')
+    fig, ax = plt.subplots()
+    ff.discretization.visualize_classic_binning(ax,
+                                             closest,
+                                             X,
+                                             log_c=False,
+                                             cmap='viridis')
+    fig.savefig('05_fact_example_original_binning_closest.png')
+    '''
+
