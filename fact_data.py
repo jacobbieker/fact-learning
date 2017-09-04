4a6a720ff9db4bdcbdc53d2d168d1a51b7654a63
Jacob Bieker <jacob@bieker.tech>
2017-09-04 14:50:06 +0200

    Update to use pull mode testing

--- fact_data.py
+++ fact_data.py
@@ -4,6 +4,7 @@ matplotlib.use('Agg')
 
 import funfolding as ff
 from funfolding import discretization
+from funfolding.pipeline import split_mc_test_unfolding
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
@@ -172,77 +173,6 @@ def SVD_Unf(model, vec_y, vec_x):
     return vec_x_est, V_x_est, vec_b, sigma_b, vec_b_est, S_values[order]
 
 
-def split_mc_test_unfolding(n_pulls,
-                            n_events_mc,
-                            n_events_test,
-                            n_events_A=-1,
-                            n_events_binning='n_events_test',
-                            random_state=None):
-    if not isinstance(random_state, np.random.RandomState):
-        random_state = np.random.RandomState(random_state)
-
-    if isinstance(n_events_test, float):
-        if n_events_test > 0 and n_events_test < 1:
-            n_events_test = n_events_mc * n_events_test
-        else:
-            n_events_test = int(n_events_test)
-    elif not isinstance(n_events_test, int):
-        raise ValueError("'n_events_test' must be either None, int or float")
-
-    if n_events_A is None:
-        n_events_A = None
-    elif isinstance(n_events_A, float):
-        n_events_A = int(n_events_mc * n_events_A)
-    elif not isinstance(n_events_A, int):
-        raise ValueError("'n_events_A' must be either None, int or "
-                         "float")
-
-    if n_events_binning is None:
-        n_events_binning = 0
-    elif isinstance(n_events_binning, float):
-        n_events_binning = int(n_events_mc * n_events_binning)
-    elif isinstance(n_events_binning, str):
-        if n_events_binning.lower() == 'n_events_test':
-            n_events_binning = int(n_events_test)
-        else:
-            raise ValueError(
-                "'{}'' unknown option for 'n_events_binning'".format(
-                    n_events_binning))
-    else:
-        raise ValueError("'n_events_binning' must be either None, int or "
-                         "float")
-
-    if (n_events_test + n_events_binning + n_events_A) > n_events_mc:
-        raise ValueError("'n_events_test' + 'n_events_binning' + 'n_events_A' "
-                         "has to be smaller than n_events_mc")
-    n_events_test_pulls = np.random.poisson(n_events_test,
-                                            size=n_pulls)
-    idx = np.arange(n_events_mc)
-
-    for n_events_test_i in n_events_test_pulls:
-        np.random.shuffle(idx)
-        test_idx = np.sort(idx[:n_events_test_i])
-        train_idx = idx[n_events_test_i:]
-
-        if n_events_binning == 0:
-            if n_events_A == -1:
-                A_idx = np.sort(train_idx)
-            else:
-                A_slice = slice(None, n_events_A)
-                A_idx = np.sort(idx[A_slice])
-            yield test_idx, A_idx
-        else:
-            binning_slice = slice(None, n_events_binning)
-            binning_idx = np.sort(idx[binning_slice])
-            if n_events_A == -1:
-                A_slice = slice(n_events_binning, None)
-            else:
-                A_slice = slice(n_events_binning,
-                                n_events_binning + n_events_A)
-            A_idx = np.sort(idx[A_slice])
-            yield test_idx, A_idx, binning_idx
-
-
 if __name__ == '__main__':
 
     def generate_data(random_state=None, noise=True, smearing=True, resolution_val=1., noise_val=0., response_bins=20,
@@ -382,7 +312,7 @@ if __name__ == '__main__':
 
 
 
-    if False:
+    if True:
         mc_data, on_data, off_data = load_gamma_subset("gamma_test.hdf5", theta2_cut=0.7, conf_cut=0.3, num_off_positions=5)
 
         # 0's are off
@@ -413,12 +343,13 @@ if __name__ == '__main__':
         list_of_closest_conditions = []
         list_of_lowest_conditions = []
 
-        #for run in testing_data:
-        for run in range(1, num_pulls_prim):
+        run = 0
+        for indicies in testing_data:
+            run += 1
             print(run)
 
             # Get the "test" vs non test data
-            df_test = on_data[10000*(run-1):10000*run]
+            df_test = on_data.loc[indicies[0]].dropna(how='all')
             df_train = on_data[~on_data.isin(df_test)].dropna(how='all')
 
             # Split into 20 /80 mix for tree/detector matrix sets
